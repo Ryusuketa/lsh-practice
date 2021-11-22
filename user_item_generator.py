db667c24df9895f4e6ee2b3d6e2bdbe69522f34d
ryusuke-tanaka <r.t433102@gmail.com>
2021-11-22 22:28:55 +0900

    user_item_generator tmp

---
+++ user_item_generator.py
@@ -0,0 +1,16 @@
+
+class UserItemInteractionGenerator:
+    def __init__(self, user_size: int, item_size: int, num_cluster: int) -> None:
+        self._user_size = user_size
+        self._item_size = item_size
+        self._num_cluster = num_cluster
+        
+    def sample(self) -> str:
+        return ''
+
+    def _define_interaction_probability(self):
+
+    def generate_items(self):
+        return
+
+    def
